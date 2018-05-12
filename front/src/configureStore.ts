73dc34f56727edd658389b0c65b689909ae05e70
t-kusakabe <ttmac0704@gmail.com>
2018-05-12 22:34:08 +0900

    redux-loggerがerror吐くので一旦ignore

--- front/src/configureStore.ts
+++ front/src/configureStore.ts
@@ -1,12 +1,14 @@
 import {createStore as reduxCreateStore, applyMiddleware, combineReducers} from 'redux'
-import logger from 'redux-logger'
+// import logger from 'redux-logger'
+import TopReducer from './modules/top'
 
 const createStore = () => {
   return reduxCreateStore(
     combineReducers({
+      top: TopReducer
     }),
     applyMiddleware(
-      logger
+      // logger
     )
   )
 }
