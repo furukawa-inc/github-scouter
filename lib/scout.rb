2b207f027ce1eafa45fa6571d80ea54dfc1d305b
Shun Furukawa <furukawa9312@gmail.com>
2018-05-12 22:33:30 +0900

    :octocat: Modify change get request pattern && add gem

--- lib/scout.rb
+++ lib/scout.rb
@@ -1,6 +1,12 @@
 require 'net/http'
+require 'mechanize'
 
-git_url = "https://github.com/users/#{USER_ID}/contributions"
-dom = Net::HTTP.get_print(URI.parse(git_url))
+# git_url = "https://github.com/users/#{USER_ID}/contributions"
+# dom = Net::HTTP.get_print(URI.parse(git_url))
 
-puts dom
+USER_ID = 't-kusakabe'
+
+agent = Mechanize.new
+dom = agent.get("https://github.com/users/#{USER_ID}/contributions")
+
+puts dom.search('svg')
