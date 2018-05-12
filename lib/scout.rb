61f8fb89890945ce84adea7b989a4a3d76e08b4e
Shun Furukawa <furukawa9312@gmail.com>
2018-05-12 22:35:13 +0900

    :octocat: Modify Modify........sorry

--- lib/scout.rb
+++ lib/scout.rb
@@ -1,11 +1,6 @@
 require 'net/http'
 require 'mechanize'
 
-# git_url = "https://github.com/users/#{USER_ID}/contributions"
-# dom = Net::HTTP.get_print(URI.parse(git_url))
-
-USER_ID = 't-kusakabe'
-
 agent = Mechanize.new
 dom = agent.get("https://github.com/users/#{USER_ID}/contributions")
 
