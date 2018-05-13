b033a205d1d8a4f9c683cec6a34c76654256e57a
t-kusakabe <ttmac0704@gmail.com>
2018-05-13 11:00:29 +0900

    Create action creator

--- front/src/modules/top.ts
+++ front/src/modules/top.ts
@@ -1,12 +1,29 @@
+// import axios from 'axios'
+
 const initialState = {
-  test: 'foo'
+  inputValue: ''
 }
 
 const reducer = (state = initialState, action: any) => {
   switch(action.type) {
+    case 'CHANGE_TEXT':
+      return Object.assign({}, state, {
+        inputValue: action.value
+      })
+    case 'POST_ID':
+      console.log(state.inputValue)
+      return
     default:
       return state
   }
 }
 
 export default reducer
+
+export const changeText = (etv: any) => {
+  return {type: 'CHANGE_TEXT', value: etv }
+}
+
+export const postId = () => {
+  return {type: 'POST_ID'}
+}
