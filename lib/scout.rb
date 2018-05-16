a3d11af7496c746e15354a605f5e979a976393fc
Shun Furukawa <furukawa9312@gmail.com>
2018-05-16 22:11:24 +0900

    :octocat: Modify scouting

--- lib/scout.rb
+++ lib/scout.rb
@@ -10,8 +10,15 @@ rects = dom.search('svg g g rect')
 
 all_fill = []
 
+def conversion(string)
+  color_code_a_digit = string
+  color_code_a_digit_index = ([*'0'..'10'] + [*'a'..'f']).index(color_code_a_digit)
+  ([*'0'..'10'] + [*'a'..'f'])[-(color_code_a_digit_index+1)]
+end
+
 rects.each do |rect|
-  all_fill << rect.get_attribute(:fill).slice!(1, 6).hex
+  all_fill << rect.get_attribute(:fill).slice!(1, 6).split('').each { |string| conversion string }.join.hex
 end
 
 puts all_fill.inject { |sum, n| sum + n }
+
